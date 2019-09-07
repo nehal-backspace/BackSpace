e8cda5319573e6a88c75e2242b659bf861be6710
nehal-backspace <53865141+nehal-backspace@users.noreply.github.com>
2019-09-07 19:30:42 +0530

    Add files via upload

---
+++ otp/controller.php
@@ -0,0 +1,58 @@
+<?php
+session_start();
+error_reporting(E_ALL & ~ E_NOTICE);
+require ('textlocal.class.php');
+
+class Controller
+{
+    function __construct() {
+        $this->processMobileVerification();
+    }
+    function processMobileVerification()
+    {
+        switch ($_POST["action"]) {
+            case "send_otp":
+                
+                $mobile_number = $_POST['mobile_number'];
+                
+                $apiKey = urlencode('/YOUR KEY');
+                $Textlocal = new Textlocal(false, false, $apiKey);
+                
+                $numbers = array(
+                    $mobile_number
+                );
+                $sender = 'TXTLCL';
+                $otp = rand(100000, 999999);
+                $_SESSION['session_otp'] = $otp;
+                $message = "Your One Time Password is " . $otp;
+                
+                try{
+                    $response = $Textlocal->sendSms($numbers, $message, $sender);
+                    require_once ("verification-form.php");
+                    exit();
+                }catch(Exception $e){
+                    die('Error: '.$e->getMessage());
+                }
+                break;
+                
+            case "verify_otp":
+                $otp = $_POST['otp'];
+                $_SESSION['mdone']="0";
+                if ($otp == $_SESSION['session_otp']) {
+                    unset($_SESSION['session_otp']);
+                    $_SESSION['mdone']="1";
+                    echo json_encode(array("type"=>"success", "message"=>"Your mobile number is verified!"));
+                     header("Location: https://backspace.ind.in/registeration/verify_email.php");
+                     die();
+                } else {
+                    $_SESSION['mdone']="0";
+                    echo json_encode(array("type"=>"error", "message"=>"Mobile number verification failed"));
+                    
+                }
+                
+                break;
+        }
+    }
+}
+$controller = new Controller();
+?>
